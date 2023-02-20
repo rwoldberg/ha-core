528ff9927e2b0632a2a3c29244362262aabbba0a
Robert Woldberg <rwodlberg@absolute.com>
2023-02-20 16:50:14 +0000

    update manifest and add options flow

--- homeassistant/components/ldata/config_flow.py
+++ homeassistant/components/ldata/config_flow.py
@@ -8,11 +8,11 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
-from .const import DOMAIN
+from .const import DOMAIN, UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT
 from .ldata_service import LDATAService
 
 _LOGGER = logging.getLogger(__name__)
@@ -43,7 +43,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
         raise CannotConnect
 
     # Return info that you want to store in the config entry.
-    return {"title": f"Leviton LDATA ({data[CONF_USERNAME]})"}
+    return {"title": f"Leviton LDATA ({data[CONF_USERNAME],data[CONF_PASSWORD]})"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -70,8 +70,44 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                }
+            ),
+            errors=errors,
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> OptionsFlow:
+        """Get the options flow for this handler."""
+        return OptionsFlow(config_entry)
+
+
+class OptionsFlow(config_entries.OptionsFlow):
+    """Handle the options flow for Leviton LDATA."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None) -> FlowResult:
+        """Return the options form."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        options = {
+            vol.Optional(
+                UPDATE_INTERVAL,
+                default=self.config_entry.options.get(
+                    UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT
+                ),
+            ): int,
+        }
+
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
 
 
 class CannotConnect(HomeAssistantError):
