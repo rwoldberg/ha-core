4b6690d8d93bf1fa707e97bd04375ec98b52c1a5
Robert Woldberg <rwodlberg@absolute.com>
2024-07-06 18:09:54 +0000

    Update init

--- homeassistant/components/ldata/__init__.py
+++ homeassistant/components/ldata/__init__.py
@@ -1,4 +1,5 @@
 """The Leviton LDATA integration."""
+
 from __future__ import annotations
 
 import asyncio
@@ -59,10 +60,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
